2892822950f78ccc5684c61b1bdec5f34f49d988
seriokim <atomyoun@gmail.com>
2022-05-15 19:38:56 +0900

    현재 위치 날씨 1

--- app/src/main/java/com/example/fixfit/MainActivity.java
+++ app/src/main/java/com/example/fixfit/MainActivity.java
@@ -1,8 +1,9 @@
+
 package com.example.fixfit;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
